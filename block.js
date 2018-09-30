136f6f24039f5f8c402e7cb5fd130772a3ffb1e0
Yerlin Matu <yerlinmatu@MacBook-Pro-de-Yerlin.local>
2018-09-30 02:09:01 -0500

    Export default Block

--- block.js
+++ block.js
@@ -1,9 +1,9 @@
-class Block {
+export default class Block {
 	constructor () {
-			this.index = 0;
-			this.previousHash = '';
-			this.hash = '';
-			this.nonce = 0;
-			this.transactions = [];
+		this.index = 0;
+		this.previousHash = '';
+		this.hash = '';
+		this.nonce = 0;
+		this.transactions = [];
 	}
 }
--- transaction.js
+++ transaction.js
@@ -1,7 +1,7 @@
 export default class Transaction {
 	constructor(from, to, amount) {
-			this.from = from;
-			this.to = to;
-			this.amount = amount;
+		this.from = from;
+		this.to = to;
+		this.amount = amount;
 	}
 }
